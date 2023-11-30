9df43b59f2ab74927730eba871d10023980a9de6
caressaputri <caressaputriyuliantoro@gmail.com>
2023-12-01 00:31:10 +0700

    tutorial 7 yang benar

--- lib/screens/menu.dart
+++ lib/screens/menu.dart
@@ -1,6 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:shopping_list/widgets/left_drawer.dart';
-
+import 'package:shopping_list/widgets/shop_card.dart';
 
 class MyHomePage extends StatelessWidget {
   MyHomePage({Key? key}) : super(key: key);
@@ -14,45 +14,36 @@ class MyHomePage extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(
-        title: const Text('Shopping List'),
-        backgroundColor: Colors.indigo,
-        foregroundColor: Colors.white,
-      ),
-      drawer: const LeftDrawer(),
-      body: SingleChildScrollView(
-        child: Padding(
-          padding: const EdgeInsets.all(10.0),
-          child: Column(
-            children: <Widget>[
-              const Padding(
-                padding: EdgeInsets.only(top: 10.0, bottom: 10.0),
-                child: Text(
-                  'PBP Shop',
-                  textAlign: TextAlign.center,
-                  style: TextStyle(
-                    fontSize: 30,
-                    fontWeight: FontWeight.bold,
-                  ),
-                ),
-              ),
-              GridView.count(
-                primary: true,
-                padding: const EdgeInsets.all(20),
-                crossAxisSpacing: 10,
-                mainAxisSpacing: 10,
-                crossAxisCount: 3,
-                shrinkWrap: true,
-                children: items.map((ShopItem item) {
-                  return ShopCard(item);
-                }).toList(),
-              ),
-            ],
-          ),
+        appBar: AppBar(
+          title: const Text("Shopping List"),
+          backgroundColor: Colors.indigo,
+          foregroundColor: Colors.white,
         ),
-      ),
-    );
+        drawer: const LeftDrawer(),
+        body: SingleChildScrollView(
+          child: Padding(
+              padding: const EdgeInsets.all(10),
+              child: Column(children: <Widget>[
+                const Padding(
+                    padding: EdgeInsets.only(top: 10, bottom: 10),
+                    child: Text("PBP Shop",
+                        textAlign: TextAlign.center,
+                        style: TextStyle(
+                          fontSize: 30,
+                          fontWeight: FontWeight.bold,
+                        ))),
+                GridView.count(
+                  primary: true,
+                  padding: const EdgeInsets.all(20),
+                  crossAxisSpacing: 10,
+                  mainAxisSpacing: 10,
+                  crossAxisCount: 3,
+                  shrinkWrap: true,
+                  children: items.map((ShopItem item) {
+                    return ShopCard(item);
+                  }).toList(),
+                )
+              ])),
+        ));
   }
 }
-
-
--- lib/widgets/left_drawer.dart
+++ lib/widgets/left_drawer.dart
@@ -1,63 +1,48 @@
 import 'package:flutter/material.dart';
-import 'package:shopping_list/menu.dart'; // Sesuaikan path menu.dart
-import 'package:shopping_list/shoplist_form.dart';
+import 'package:shopping_list/screens/menu.dart';
+import 'package:shopping_list/screens/shoplist_form.dart';
 
 class LeftDrawer extends StatelessWidget {
-  const LeftDrawer({Key? key});
+  const LeftDrawer({super.key});
 
   @override
   Widget build(BuildContext context) {
     return Drawer(
       child: ListView(
         children: [
-          DrawerHeader(
-            decoration: BoxDecoration(
-              color: Colors.indigo,
-            ),
-            child: Column(
-              children: [
-              Text(
-              'Shopping List',
-              textAlign: TextAlign.center,
-              style: TextStyle(
-                fontSize: 30,
-                fontWeight: FontWeight.bold,
-                color: Colors.white,
+          const DrawerHeader(
+              decoration: BoxDecoration(
+                color: Colors.indigo,
               ),
-            ),
-            Padding(padding: EdgeInsets.all(10),
-              Text(
-                "Catat seluruh keperluan belanjamu di sini!",
-                textAlign: TextAlign.center,
-                style: TextStyle(
-                  fontSize: 15,
-                  fontWeight: FontWeight.normal,
-                  color: Colors.white,
-                ),
-              ),
-              ],
-            ),
-          ),
+              child: Column(
+                children: [
+                  Text('Shopping List',
+                      textAlign: TextAlign.center,
+                      style: TextStyle(
+                          fontSize: 30,
+                          fontWeight: FontWeight.bold,
+                          color: Colors.white)),
+                  Padding(padding: EdgeInsets.all(10)),
+                  Text('Catat seluruh keperluan belanjamu di sini!',
+                      textAlign: TextAlign.center,
+                      style: TextStyle(fontSize: 15, color: Colors.white))
+                ],
+              )),
           ListTile(
-            leading: Icon(Icons.home_outlined),
-            title: Text('Halaman Utama'),
-            onTap: () {
-              Navigator.pushReplacement(
-                context,
-                MaterialPageRoute(
-                  builder: (context) => MyHomePage(), // Pastikan ini adalah halaman utama yang benar
-                ),
-              );
-            },
-          ),
+              leading: const Icon(Icons.home_outlined),
+              title: const Text('Halaman Utama'),
+              onTap: () {
+                Navigator.of(context).pushReplacement(
+                    MaterialPageRoute(builder: (context) => MyHomePage()));
+              }),
           ListTile(
-            leading: Icon(Icons.add_shopping_cart),
-            title: Text('Tambah Produk'),
+            leading: const Icon(Icons.add_shopping_cart),
+            title: const Text('Tambah Produk'),
             onTap: () {
-              // Tambahkan routing ke halaman ShopFormPage setelah dibuat
-              Navigator.of(context).pushReplacement(MaterialPageRoute(builder: (context) => const ShopFormPage()));
+              Navigator.of(context).pushReplacement(MaterialPageRoute(
+                  builder: (context) => const ShopFormPage()));
             },
-          ),
+          )
         ],
       ),
     );
